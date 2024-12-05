0d1775f2cc32bda5437c5c16719ec12e4fe668cc
Cacahuetedu62 <rogez.aurore01@gmail.com>
2024-12-05 09:50:12 +0100

    Ajout de l'index

---
+++ templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="index, follow">
+    <meta name="description" content="EcoRide, quand économie rime avec écologie ! le covoiturage éléctrique, découvrez le covoiturage électrique pour des trajets plus verts et économiques.">
+    <title>EcoRide | Covoiturage écologique et éléctrique</title>
+</head>
