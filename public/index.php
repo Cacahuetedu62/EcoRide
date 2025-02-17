1665769330dd2d1cd0de060a88a591697c49d789
Cacahuetedu62 <rogez.aurore01@gmail.com>
2025-02-17 15:21:24 +0100

    ajout des require once

--- public/index.php
+++ public/index.php
@@ -2,21 +2,37 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo "1. Début du script<br>";
-
 // Charger l'autoloader de Composer
 require_once __DIR__ . '/../vendor/autoload.php';
-
-echo "2. Autoloader chargé<br>";
 
 // Charger la configuration
 require_once __DIR__ . '/../lib/config.php';
 require_once __DIR__ . '/../lib/pdo.php';
 
-echo "3. Configuration et PDO chargés<br>";
+// Charger le header
+require_once __DIR__ . '/../templates/header.php';
 
-$index_content = file_get_contents(__DIR__ . '/../index.php');
-echo "4. Contenu de index.php :<br><pre>" . htmlspecialchars($index_content) . "</pre><br>";
+// Obtenir l'URI
+$request_uri = $_SERVER['REQUEST_URI'];
+$clean_uri = strtok($request_uri, '?');
+$clean_uri = rtrim($clean_uri, '/');
 
-// Charger le fichier
-require_once __DIR__ . '/../index.php';
+// Charger la page principale
+if (empty($clean_uri) || $clean_uri === '/') {
+    require_once __DIR__ . '/../index.php';
+} else {
+    $file = __DIR__ . '/../' . ltrim($clean_uri, '/');
+    if (!str_ends_with($file, '.php')) {
+        $file .= '.php';
+    }
+    
+    if (file_exists($file)) {
+        require_once $file;
+    } else {
+        http_response_code(404);
+        echo "Page non trouvée";
+    }
+}
+
+// Charger le footer
+require_once __DIR__ . '/../templates/footer.php';
