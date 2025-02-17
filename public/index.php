cd44e17d64dbbd7c2499696c36fbf057ce3e2e86
Cacahuetedu62 <rogez.aurore01@gmail.com>
2025-02-17 15:16:11 +0100

    débeug index et style

--- public/index.php
+++ public/index.php
@@ -1,7 +1,4 @@
 <?php
-// Activez la compression Gzip
-if (!ob_start("ob_gzhandler")) ob_start();
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -18,33 +15,8 @@ require_once __DIR__ . '/../lib/pdo.php';
 
 echo "3. Configuration et PDO chargés<br>";
 
-// Obtenir l'URI complète
-$request_uri = $_SERVER['REQUEST_URI'];
-echo "4. URI demandée : " . $request_uri . "<br>";
+$index_content = file_get_contents(__DIR__ . '/../index.php');
+echo "4. Contenu de index.php :<br><pre>" . htmlspecialchars($index_content) . "</pre><br>";
 
-// Nettoyer l'URI
-$clean_uri = strtok($request_uri, '?');
-$clean_uri = rtrim($clean_uri, '/');
-echo "5. URI nettoyée : " . $clean_uri . "<br>";
-
-// Définir le chemin du fichier
-if (empty($clean_uri) || $clean_uri === '/') {
-    $file = __DIR__ . '/../index.php';
-} else {
-    $file = __DIR__ . '/../' . ltrim($clean_uri, '/');
-    if (!str_ends_with($file, '.php')) {
-        $file .= '.php';
-    }
-}
-
-echo "6. Fichier à charger : " . $file . "<br>";
-echo "7. Le fichier existe ? : " . (file_exists($file) ? 'Oui' : 'Non') . "<br>";
-
-if (file_exists($file)) {
-    echo "8. Chargement du fichier...<br>";
-    require_once $file;
-    echo "9. Fichier chargé<br>";
-} else {
-    http_response_code(404);
-    echo "8. Page non trouvée<br>";
-}
+// Charger le fichier
+require_once __DIR__ . '/../index.php';
