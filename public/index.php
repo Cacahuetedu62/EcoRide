28e858c2a94f6df42a2abe85951e20bb064cc4cc
Cacahuetedu62 <rogez.aurore01@gmail.com>
2025-02-17 13:10:26 +0100

    essais 2

--- public/index.php
+++ public/index.php
@@ -1,22 +1,3 @@
 <?php
-// Active la compression Gzip 
-if (!ob_start("ob_gzhandler")) ob_start();  
-
-// Charger l'autoloader de Composer en premier 
-require_once __DIR__ . '/../vendor/autoload.php'; 
-require_once __DIR__ . '/../config/config.php';  
-
-// Gestion de la réécriture d'URL
-if ($_SERVER['REQUEST_URI'] != '/' && !file_exists($_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'])) {
-    // Routage pour les routes non existantes
-    include 'index.php';
-}
-
-// Vérifie et utilise le port Heroku
-$port = getenv('PORT') ?: 5000;
-
-// Le reste de votre code...
-echo "Application démarrée sur le port $port";
-
-// Votre logique principale de routage ici
-echo "Bienvenue sur EcoRide !";
+echo "Démarrage réussi !";
+exit;
